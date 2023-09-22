cf25bed14b12d441fecce41b6919b0d6218d76fe
CortiEnea <enea.corti@samtrevano.ch>
2023-09-22 11:29:56 +0200

    Fine mattinata 2023.09.22, Aggiornato diario e codice che stampa parole orizzontali

--- 5_Applicativo/Parole/parole.js
+++ 5_Applicativo/Parole/parole.js
@@ -1,4 +1,6 @@
-var prova;
+var listaParole;
+
+
 
 function readFile() {
   var fileInput = document.getElementById('dizionario');
@@ -7,10 +9,12 @@ function readFile() {
   reader.onload = function (e) {
     var parole = e.target.result;
 
-    prova = parole.split('\n');
+    listaParole = parole.split('\n');
 
-    console.log(prova);
+
+
     //giraParola();
+
     stampa();
   };
 
@@ -18,72 +22,70 @@ function readFile() {
 
 }
 
-
+// funzione per la stampa delle parole 
 function stampa() {
 
 
-for(var i = 0; i < prova.length; i++){
+  for (var i = 0; i < listaParole.length; i++) {
 
-  if(i < 9){
+    if (i < 9) {
 
-    document.getElementById('output').innerHTML += prova[i] + "<br>";
+      document.getElementById('output').innerHTML += listaParole[i] + "<br>";
 
-  }
-  
-
-  if(i >= 10 && i < 19){
-
-    document.getElementById('output2').innerHTML += prova[i] + "<br>";
-
-
- }else if(i >= 20 && i < 29){
-
-    document.getElementById('output3').innerHTML += prova[i] + "<br>";
-  }else if(i >= 30 && i < 39){
-
-    document.getElementById('output4').innerHTML += prova[i] + "<br>";
-  }else if(i >= 40 && i < 49){
-
-    document.getElementById('output5').innerHTML += prova[i] + "<br>";
-  }
-
-
-  
-
-  griglia();
-}
-
-
-}
-
-
-
-function griglia(){
-  var table = "<table>";
-    for(var i = 0; i < 25; i++){
-        table += "<tr>";
-        for(var j = 0; j < 25; j++){
-            table += "<td> </td>";
-        }
-        table += "</tr>";
     }
-    table += "</table>";
-    document.getElementById('si').innerHTML = table;
+
+
+    if (i >= 10 && i < 19) {
+
+      document.getElementById('output2').innerHTML += listaParole[i] + "<br>";
+
+
+    } else if (i >= 20 && i < 29) {
+
+      document.getElementById('output3').innerHTML += listaParole[i] + "<br>";
+    } else if (i >= 30 && i < 39) {
+
+      document.getElementById('output4').innerHTML += listaParole[i] + "<br>";
+    } else if (i >= 40 && i < 49) {
+
+      document.getElementById('output5').innerHTML += listaParole[i] + "<br>";
+    }
+
+
+  }
+  griglia();
+
+  generaArray();
+}
+
+
+function griglia() {
+
+
+  var table = "<table id='table'>";
+  for (var i = 0; i < 25; i++) {
+    table += "<tr>";
+    for (var j = 0; j < 25; j++) {
+      table += "<td> </td>";
+    }
+    table += "</tr>";
+  }
+  table += "</table>";
+
+
 
 
 }
 
 
+function giraParola() {
 
-function giraParola(){
+  for (var i = 0; i < 20; i++) {
 
-  for(var i = 0; i < 20;i++){
-    
-    var ran =   Math.floor(Math.random() * 50);
-    console.log(ran);
-    prova[ran] = reverseString(prova[ran]);
+    var ran = Math.floor(Math.random() * 50);
+
+    listaParole[ran] = reverseString(listaParole[ran]);
   }
-
 
 }
 
@@ -93,8 +95,53 @@ function reverseString(str) {
 }
 
 
-function riempi(){
-  
-}
+function generaArray() {
+  var a = new Array(25);
 
+  for (var s = 0; s < a.length; s++) {
+    a[s] = new Array(25);
+  }
+
+
+  for (var col = 0; col < 10; col++) {
+    var parolaRan = Math.floor(Math.random() * 1000);
+    parolaRan = listaParole[parolaRan];
+    parolaRan = parolaRan.toUpperCase();
+    var x = Math.floor(Math.random() * (25 - parolaRan.length));
+    var y = Math.floor(Math.random() * (25 - parolaRan.length));
+
+
+    for (var riga = 0; riga < parolaRan.length; riga++) {
+      // console.log(parolaRan.length);
+      // console.log(parolaRan);
+      // console.log(parolaRan[riga]);
+
+      console.log(`parola ${parolaRan}, riga ${riga}, col ${col}, len ${parolaRan.length}`);
+
+      a[x][y] = parolaRan[riga];
+      y++;
+    }
+
+  }
+
+
+
+  var table = "<table>";
+  for (var col = 0; col < a.length; col++) {
+    table += "<tr>";
+    for (var riga = 0; riga < a[col].length; riga++) {
+      if (a[col][riga] == undefined) {
+        table += "<td>-</td>";
+      } else {
+        table += "<td>" + a[col][riga] + "</td>";
+      }
+
+    }
+    table += "</tr>";
+  }
+  table += "</table>";
+  document.getElementById('si').innerHTML = table;
+
+
+}
 
